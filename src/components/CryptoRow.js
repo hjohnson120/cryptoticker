00007ad27a53f6c810d58b3314e9771d0870c597
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-05-22 09:17:23 -0400

    background color

--- src/components/CryptoRow.js
+++ src/components/CryptoRow.js
@@ -3,19 +3,13 @@ import Pictures from './Pictures'
 class CryptoRow extends Component {
   render() {
     return (
-      <div>
-        <section className="table">
-          <ul>
-            <li>
-              <h3> {this.props.name}</h3>
-              <p>
-                <Pictures id={this.props.id} />
-              </p>
-              <em>{this.props.currencies}</em>
-            </li>
-          </ul>
-        </section>
-      </div>
+      <section className="table">
+        <h3> {this.props.name}</h3>
+        <p>
+          <Pictures id={this.props.id} />
+        </p>
+        <em>{this.props.currencies}</em>
+      </section>
     )
   }
 }
